f804677d8211bf7a18fc1d3261486e92b83e5a6e
CodeByJJY <optimaprime@naver.com>
2024-10-25 10:20:55 +0900

    Title: 왔다 갔던 구역 2, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241025/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,23 @@
+street = [0 for _ in range(2000)]
+
+n = int(input())
+start = 999
+cnt = 0
+
+for _ in range(n):
+    instr = input()
+    scalar = int(instr[0])
+
+    if instr[2] == 'L':
+        for i in range(start - 1, start - scalar - 1, -1):
+            street[i] += 1
+        start -= scalar
+    else:
+        for i in range(start, start + scalar):
+            street[i] += 1
+        start += scalar
+
+for i in street:
+    if i >= 2:  cnt += 1
+
+print(cnt)
