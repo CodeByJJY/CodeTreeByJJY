997d5553c9cd6fd94c8f8ed634b22d7d77b67a24
CodeByJJY <optimaprime@naver.com>
2024-11-01 13:03:43 +0900

    Update n-permutations-of-k-with-repetition.py

--- 241101/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 241101/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,27 +1,27 @@
-# arr = []
+################ 내 풀이 ##################
+arr = []
 
-# def choose(K, N):
-#     #   종료 조건
-#     #   길이가 N일 때 출력 후 되돌아가기.
-#     if N == 0:
-#         print(*arr)
-#         return
+def choose(K, N):
+    #   종료 조건
+    #   길이가 N일 때 출력 후 되돌아가기.
+    if N == 0:
+        print(*arr)
+        return
 
-#     #   진입
-#     for num in range(1, K+1):
-#         arr.append(num)
-#         choose(K, N-1)
-#         arr.pop()
+    #   진입
+    for num in range(1, K+1):
+        arr.append(num)
+        choose(K, N-1)
+        arr.pop()
 
 
-# #   1<=K<=8,    1<=N<=8
-# K, N = map(int, input().split())
-# choose(K, N)
+#   1<=K<=8,    1<=N<=8
+K, N = map(int, input().split())
+choose(K, N)
 
 
 
 ################ 강사님 풀이 ##################
-
 K, N = map(int, input().split())
 
 arr = []
