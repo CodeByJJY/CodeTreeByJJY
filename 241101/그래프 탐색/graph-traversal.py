83e9986a4c17016e3add6dff8f0d3e3960d60a42
CodeByJJY <optimaprime@naver.com>
2024-11-01 20:35:24 +0900

    Title: 그래프 탐색, Time: 94ms, Memory: 19MB, Status: Passed - CodeTree

--- 241101/그래프 탐색/graph-traversal.py
+++ 241101/그래프 탐색/graph-traversal.py
@@ -3,13 +3,13 @@
 # 방문한 정점의 개수
 
 N, M = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
+graph = [[] for _ in range(N + 1)]          # 하나의 vertex에 연결된 다른 vertex 파악.
 for _ in range(M):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-visited = [False] * (N + 1)
+visited = [False] * (N + 1)                 # vertex 목록은 1차원 배열로 표현 가능.
 
 def dfs(now):
     visited[now] = True
@@ -20,7 +20,7 @@ def dfs(now):
 dfs(1)
 
 cnt = 0
-for b in visited[2:]:
+for b in visited[2:]:                       # 다시 원점으로 돌아온 것은 제외.
     if b:
         cnt += 1
 
