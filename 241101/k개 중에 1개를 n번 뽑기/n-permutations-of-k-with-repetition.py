7430970e5fdcf71036f7b749f7a24cdb1fcb849d
CodeByJJY <optimaprime@naver.com>
2024-11-01 13:02:44 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 345ms, Memory: 29MB, Status: Passed - CodeTree

--- 241101/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 241101/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,21 +1,39 @@
+# arr = []
+
+# def choose(K, N):
+#     #   종료 조건
+#     #   길이가 N일 때 출력 후 되돌아가기.
+#     if N == 0:
+#         print(*arr)
+#         return
+
+#     #   진입
+#     for num in range(1, K+1):
+#         arr.append(num)
+#         choose(K, N-1)
+#         arr.pop()
+
+
+# #   1<=K<=8,    1<=N<=8
+# K, N = map(int, input().split())
+# choose(K, N)
+
+
+
+################ 강사님 풀이 ##################
+
+K, N = map(int, input().split())
+
 arr = []
 
-def choose(K, N):
-    #   종료 조건
-    #   길이가 N일 때 출력 후 되돌아가기.
-    if N == 0:
+def choose(curr_num) :
+    if curr_num == N+1:
         print(*arr)
         return
-
-    #   진입
-    for num in range(1, K+1):
-        arr.append(num)
-        choose(K, N-1)
+    
+    for i in range(1, K+1):
+        arr.append(i)
+        choose(curr_num + 1)
         arr.pop()
 
-
-
-
-#   1<=K<=8,    1<=N<=8
-K, N = map(int, input().split())
-choose(K, N)
+choose(1)
