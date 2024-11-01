46e6d87a219b4cf0d3639cebc9ed5dd16c5de537
CodeByJJY <optimaprime@naver.com>
2024-11-01 13:57:58 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241101/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -0,0 +1,24 @@
+# 조건 : 연속하여 같은 숫자가 3번 나오면 안된다.
+
+answer = []
+
+# 1 <= K <= 4,  1 <= N <= 8
+K, N = map(int, input().split())
+
+
+def choose(curr_num):
+    if curr_num == N + 1:
+        print(*answer)
+        return
+        
+    # 조건문
+    # 1. 해당 시점까지 완성된 answer 배열 길이가 3보다 작거나
+    # 2. 현재 숫자가 전, 전전 숫자와는 다를 경우
+    # 결론 : 배열에 숫자 추가
+    for i in range(1, K + 1):
+        if curr_num <= 2 or answer[curr_num-2] == answer[curr_num-3] != i:
+            answer.append(i)
+            choose(curr_num + 1)
+            answer.pop()
+
+choose(1)
