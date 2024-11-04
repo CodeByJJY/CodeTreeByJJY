94ad9620908f23aa0ed235a6c00c058deedaaa58
CodeByJJY <optimaprime@naver.com>
2024-11-04 19:50:17 +0900

    Title: 사각형 채우기 3, Time: 85ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241104/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,18 @@
+# 입력
+n = int(input())
+
+# Bottom-up
+dp = [0] * (n + 1)
+
+if n == 1:
+    print(2)
+    exit()
+elif n == 2:
+    print(7)
+    exit()
+
+dp[1], dp[2] = 2, 7
+for _ in range(3, n + 1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n] % 1000000007)
