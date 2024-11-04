184992e5118a361f8e35a369daf4d409ae1ed5a3
CodeByJJY <optimaprime@naver.com>
2024-11-04 18:01:31 +0900

    Title: 사각형 채우기, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

--- 241104/사각형 채우기/rectangle-fill.py
+++ 241104/사각형 채우기/rectangle-fill.py
@@ -10,8 +10,8 @@ elif n == 2:
     exit()
 
 # 점화식
-dp[1], dp[2] = 1, 1
+dp[1], dp[2] = 1, 2
 for i in range(3, n + 1):
-    dp[i] = 2 * dp[i - 2] + dp[i - 1]
+    dp[i] = dp[i - 2] + dp[i - 1]
 
 print(dp[n]%10007)
