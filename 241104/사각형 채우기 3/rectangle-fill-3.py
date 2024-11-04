39876f7024d6dc1ff28c93f4bad20f99a1bdb826
CodeByJJY <optimaprime@naver.com>
2024-11-04 20:03:05 +0900

    Title: 사각형 채우기 3, Time: 55ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241104/사각형 채우기 3/rectangle-fill-3.py
+++ 241104/사각형 채우기 3/rectangle-fill-3.py
@@ -1,9 +1,9 @@
+MOD = 1000000007  # 결과를 나눌 모듈러 값
+
 # 입력
-n = int(input())
+n = int(input("n을 입력하세요: "))
 
-# Bottom-up
-dp = [0] * (n + 1)
-
+# 예외 처리: n이 1 또는 2인 경우의 결과를 바로 출력
 if n == 1:
     print(2)
     exit()
@@ -11,8 +11,13 @@ elif n == 2:
     print(7)
     exit()
 
+# DP 배열 초기화
+dp = [0] * (n + 1)
 dp[1], dp[2] = 2, 7
-for i in range(3, n + 1):
-    dp[i] = 4 * dp[i-1] + 3 * dp[i-2]
 
-print(dp[n] % 1000000007)
+# 점화식을 이용하여 DP 배열 채우기
+for i in range(3, n + 1):
+    dp[i] = (2 * dp[i - 1] + dp[i - 2]) % MOD
+
+# 결과 출력
+print(dp[n])
