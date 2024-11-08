68c648e8b85dcc98f7c6d349b4c5447385fccc6b
CodeByJJY <optimaprime@naver.com>
2024-11-08 14:38:48 +0900

    Title: 동전 거슬러주기, Time: 59ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241108/동전 거슬러주기/coin-change.py
@@ -0,0 +1,16 @@
+# dp[i]: i원을 만들기 위한 최소 동전의 수.
+
+n, m = map(int, input().split())
+coin = list(map(int, input().split()))
+dp = [-1] * (m + 1)
+dp[0] = 0
+
+
+for i in range(1, m + 1):
+    for cost in coin:
+        # (i-cost)원을 만드는 최소 동전의 수 + 1
+        if i >= cost and dp[i - cost] != -1:
+            if dp[i] == -1 or dp[i] > dp[i-cost] + 1:
+                dp[i] = dp[i-cost] + 1
+
+print(dp[m])
