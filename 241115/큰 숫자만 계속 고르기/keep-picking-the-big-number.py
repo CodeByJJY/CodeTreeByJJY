275c7ec52e5fe6cf5c7668bc9a4a180bb20ae7dc
CodeByJJY <optimaprime@naver.com>
2024-11-15 21:43:43 +0900

    Title: 큰 숫자만 계속 고르기, Time: 155ms, Memory: 28MB, Status: Passed - CodeTree

--- 241115/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
+++ 241115/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
@@ -1,19 +1,18 @@
 import heapq
-import sys
 
-# 입력 처리
-n, m = map(int, sys.stdin.readline().split())
-nums = list(map(int, sys.stdin.readline().split()))
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
 
-# 최대 힙 생성
-max_heap = []
-for num in nums:
-    heapq.heappush(max_heap, -num)  # 부호 반전하여 최대 힙처럼 사용
+q = []
 
-# m번 작업 수행
-for _ in range(m):
-    largest = -heapq.heappop(max_heap)  # 최댓값 가져오기 (부호 복원)
-    heapq.heappush(max_heap, -(largest - 1))  # 감소 후 다시 삽입
+for elem in A:
+    heapq.heappush(q, -elem)
 
-# 최댓값 출력
-print(-max_heap[0])  # 힙의 최댓값 (부호 복원)
+for _ in range(M):
+    #1. 최대값 뽑기
+    elem = -heapq.heappop(q)
+
+    #2. elem-1 넣기
+    heapq.heappush(q, -(elem-1))
+
+print(-q[0])
