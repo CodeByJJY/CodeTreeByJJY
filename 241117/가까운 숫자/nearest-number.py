d8a78208ec3c8ed5a75fc9ae990d2ac5b1d6aa50
CodeByJJY <optimaprime@naver.com>
2024-11-17 18:59:20 +0900

    Title: 가까운 숫자, Time: 847ms, Memory: 31MB, Status: Passed - CodeTree

--- 241117/가까운 숫자/nearest-number.py
+++ 241117/가까운 숫자/nearest-number.py
@@ -8,16 +8,24 @@ num = list(map(int, sys.stdin.readline().split()))
 # 초기화
 s = SortedSet()
 s.add(0)
+min_len = sys.maxsize
 
-min_length = sys.maxsize
-
+# 점 추가 및 거리 계산
 for idx, val in enumerate(num):
-    s.add(val)
+    if idx == 0:
+        # 새 점 추가
+        s.add(val)
+        min_len = s[1] - s[0]
+        print(min_len)
+    else:
+        # 새 점 추가
+        s.add(val)
 
-    for point in s[:-1]:
-        r_idx = s.bisect_right(point)
+        # 이웃 점 계산
+        idx = s.index(val)  # 새 점의 위치
+        left_dist = float('inf') if idx == 0 else val - s[idx - 1]
+        right_dist = float('inf') if idx == len(s) - 1 else s[idx + 1] - val
 
-        if min_length > s[r_idx] - point:
-            min_length = s[r_idx] - point
-    
-    print(min_length)
+        # 최소 거리 출력
+        min_len = min(left_dist, right_dist, min_len)
+        print(min_len)
